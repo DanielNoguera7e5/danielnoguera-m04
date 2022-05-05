bb83736b9b53200e7585bb171d40983584a6aef5
DanielNoguera7e5 <95020245+DanielNoguera7e5@users.noreply.github.com>
2022-05-05 14:00:20 +0200

    Add files via upload

--- AdivinaCodigoSecreto-Enunciado/script.js
+++ AdivinaCodigoSecreto-Enunciado/script.js
@@ -1,6 +1,6 @@
 const codigo = [];
 const maxIntento = 8;
-var intento = 0
+var intento = 0;
 
 /*1. Genera una constante CODIGO_SECRETO de tipo array de 5 número aleatorios entre 0 y 9 usando la libreria Math.random();*/
 function codigoSecreto() {
@@ -16,34 +16,50 @@ function userInput() {
 }
 
 function inputSonNums() {
-    let esNum = true
+    let esNum = true;
     for (let num of userInput()) {
         if (isNaN(num)) { //isNaN retorna fals si el valor és un nombre
-            esNum = false
+            esNum = false;
         }
     }
-    return esNum
+    return esNum;
 }
 
 function Comprobar() {
-    document.getElementById("numero").focus()
-    validInput() 
-    info()
+    document.getElementById("numero").focus();
+    validInput();
+    info();
+    generarFila();
     
 }
 
 function validInput() {
     if (userInput().length == 5 && inputSonNums()) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
+
+function generarFila() {
+    let nuevaFila = document.createElement("div");
+    nuevaFila.classList.add("rowResult w100 flex wrap");
+    document.getElementById("Result").appendChild(nuevaFila);
+
+    for(let i = 0; i < 5; i++) {
+        
+        let div1 = document.createElement("div");
+        div1.classList.add("w20");
+        
+        
+    }
+}
+
+
 
 function info() {
 
 }
-
 
 
 function main() {
